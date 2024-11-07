ef59b7a39ff3311463bdca02453c0a3e3ad32222
Sameer Shaik <sameer.kattubadi@gmail.com>
2024-11-07 14:48:14 +0530

    fix: codebase setup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,9 @@
+// import { useParallax } from "react-scroll-parallax";
+
 export default function App() {
-  return <div>Sameer Portfolio</div>;
+  return (
+    <div className="w-[100vw] h-[100vh] flex justify-center items-center overflow-y-scroll overflow-x-hidden">
+      App
+    </div>
+  );
 }
